f2542adefee92edc446cbaaf33684355f3cf4dd0
Roel Lambers <roel.lambers@youngcolfield.nl>
2017-01-12 16:09:13 +0100

    class bordmaken gemaakt

---
+++ STS/OnlineChess/src/main/java/com/example/bewerking/BordMaken.java
@@ -0,0 +1,48 @@
+package com.example.bewerking;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.database.Bord;
+import com.example.database.SchaakVeld;
+import com.example.repository.BordManipulatie;
+import com.example.repository.VeldManipulatie;
+
+@RestController
+public class BordMaken {
+	@Autowired
+	private VeldManipulatie dataVeld;
+	@Autowired
+	private BordManipulatie dataBord;
+	
+	@RequestMapping("bordmaken")
+	public Bord createBord(){
+		Bord bord = new Bord();
+		List<SchaakVeld> lijst = new ArrayList<SchaakVeld>();
+		
+		for(int xcoord=0;xcoord<8;xcoord++){
+			for(int ycoord=0;ycoord<8;ycoord++){
+				List<Integer> coords = new ArrayList<Integer>();
+				coords.add(xcoord); coords.add(ycoord); 
+				lijst.add(createVeld(coords));
+			}
+		}
+		
+		bord.setVelden(lijst);
+		
+		return dataBord.save(bord);		
+	}
+	
+	
+	@RequestMapping("veldmaken")
+	public SchaakVeld createVeld(List<Integer> coords){
+		SchaakVeld veld = new SchaakVeld();
+		veld.setCoords(coords);
+		veld.setVakNaam("Veld" + coords.get(0) + "" + coords.get(1));
+		return dataVeld.save(veld);
+	}
+		
+}
